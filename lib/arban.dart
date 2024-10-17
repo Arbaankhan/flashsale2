ce16512cf0f818973a708ae13f1ad95d70589c5f
ARBAAN <imarbaan17@gmail.com>
2024-10-17 08:46:48 -0700

    update

---
+++ lib/arban.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Arbaan extends StatefulWidget {
+  const Arbaan({super.key});
+
+  @override
+  State<Arbaan> createState() => _ArbaanState();
+}
+
+class _ArbaanState extends State<Arbaan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
